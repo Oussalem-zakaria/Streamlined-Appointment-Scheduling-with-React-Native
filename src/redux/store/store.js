a2314ddc37c56bdf2f62b3e971d2d8bdc32a6872
Oussalem-zakaria <zakaryaoussalem@gmail.com>
2024-04-28 23:07:02 +0100

    Refactor Redux imports and remove unused code

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rdvReducer from "../features/rdvs/rdvSlice"
-import centreReduce from "../features/centres/centreSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import rdvReducer from "../features/rdvs/rdvSlice";
+import centreReduce from "../features/centres/centreSlice";
 
 export const store = configureStore({
   reducer: {
     rdvs: rdvReducer,
-    centres: centreReduce
+    centres: centreReduce,
   },
-})
+});
